871d48654719536c61217fdeb8e800308a8b847a
Behrooz Jadidi <bjadidi@ryerson.ca>
2020-06-22 20:04:21 -0400

    Create Question_1

---
+++ PS3_Q1_Jadidi_Behrooz_501001145.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include <math.h>
+#include<cmath>
+#include <fstream>
+using namespace std;
+/*////////////////Begin_Header///////////////////////////////////////////////////
+================================================================================
+// This C++ code was written by Behrooz Jadidi_Student ID:501001145
+// The first version was generated on MM_DD_YYYY
+// This code was written as a solution to AE/ME8112 - Problem_Set3_Question1
+// This code solves 1D convection/diffusion problem governed by: d/dx(puf)=d/dx(Ldf/dx)
+// In the domain 0<=x<=l , f(0)=1 and f(l)=0
+================================================================================
+/*///////////////End_Header//////////////////////////////////////////////////////
+
+//Global variables
+//Geometry
+#define N 20
+#define lx 1
+float A[N][N],b[N],T[N];
+float deltax;
+int time_step;
+
+
+//Decleration of Functions
+
+//
+
+////////////////Begin_Main code///////////////////////////////////////////////////
+int main()
+{	
+//Variables_Decleration
+
+return 0;
+}
+///////////////End_Main code//////////////////////////////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+//===========================FUNCTIONS===========================================
+/////////////////////////////////////////////////////////////////////////////////
+
+/*////////////////Begin_#///////////////////////////////////////////////////
+
+/*///////////////End_#//////////////////////////////////////////////////////
+
+
